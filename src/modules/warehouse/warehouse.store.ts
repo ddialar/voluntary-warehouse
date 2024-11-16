92d1e71afcfa0fa52f52c423fb517531fb6c292e
ddialar <ddialar@gmail.com>
2024-11-16 18:08:27 +0000

    chore(modules): connect the new warehouse creation procedure to the persistance layer

--- src/modules/warehouse/warehouse.store.ts
+++ src/modules/warehouse/warehouse.store.ts
@@ -1,7 +1,17 @@
 import { db } from '@db'
-import { Warehouse } from './warehouse.model'
+import { NewWarehouse, Warehouse } from './warehouse.model'
 
 const Entity = 'warehouse'
+
+const createWarehouse = async (warehouse: NewWarehouse): Promise<Warehouse> => {
+  const { data, error } = await db.from(Entity).insert(warehouse).select().limit(1).single().throwOnError()
+
+  if (error) {
+    console.error('Error creating warehouse:', error.message)
+  }
+
+  return data as Warehouse
+}
 
 const getWarehouses = async (): Promise<Array<Warehouse>> => {
   const { data, error } = await db.from(Entity).select().throwOnError()
@@ -13,6 +23,50 @@ const getWarehouses = async (): Promise<Array<Warehouse>> => {
   return data as Array<Warehouse>
 }
 
+interface GetWarehousesCreatedTodayByUserIdParams {
+  createdBy: string
+  startDate: Date
+  endDate: Date
+}
+
+const getLastCreatedWarehouse = async (): Promise<Warehouse | null> => {
+  try {
+    const { data, error } = await db.from(Entity).select().order('createdAt', { ascending: false }).limit(1).single()
+
+    if (error) throw error
+
+    return <Warehouse>data || null
+  } catch (error) {
+    console.error('Error fetching last created warehouse', error)
+    throw error
+  }
+}
+
+const countWarehousesCreatedTodayByUserId = async ({
+  createdBy,
+  startDate,
+  endDate
+}: GetWarehousesCreatedTodayByUserIdParams): Promise<number> => {
+  try {
+    const { count, error } = await db
+      .from(Entity)
+      .select('*', { count: 'exact', head: true })
+      .eq('createdBy', createdBy)
+      .gte('createdAt', startDate.toISOString())
+      .lte('createdAt', endDate.toISOString())
+
+    if (error) throw error
+
+    return count || 0
+  } catch (error) {
+    console.error('Error fetching warehouses by date range:', error)
+    throw error
+  }
+}
+
 export const WarehouseStore = {
-  getWarehouses
+  createWarehouse,
+  getWarehouses,
+  getLastCreatedWarehouse,
+  countWarehousesCreatedTodayByUserId
 }
