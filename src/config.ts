6ca06856b85f115c345b62776b7f91ed33fd0516
ddialar <ddialar@gmail.com>
2024-11-16 11:10:57 +0000

    chore(lib): define the database library file and its required configurations

--- src/config.ts
+++ src/config.ts
@@ -9,3 +9,6 @@ export const API_BASE = '/api'
 export const WAREHOUSE_API_URI = API_BASE.concat('/warehouse')
 
 export const MAX_AMOUNT_OF_WAREHOUSES_PER_DAY = Number(process.env.NEXT_PUBLIC_MAX_AMOUNT_OF_WAREHOUSES_PER_DAY || '1')
+
+export const DATABASE_URL = process.env.DATABASE_URL || ''
+export const DATABASE_API_KEY = process.env.DATABASE_API_KEY || ''
