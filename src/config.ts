bac09b694a0f327b29c6aaf1239fbbeea187e2a3
ddialar <ddialar@gmail.com>
2024-11-19 06:15:44 +0000

    chore(config): define new route for enroll user at warehouse

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,11 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || ''
 export const AVAILABLE_LOCALES = ['es', 'ca', 'en']
 export const DEFAULT_LOCALE = process.env.NEXT_PUBLIC_DEFAULT_LOCALE || 'es'
 
+export const DEFAULT_LOCATION: [number, number] = [39.394972, -0.411931]
+
 export const API_BASE = '/api'
 export const WAREHOUSE_API_URI = API_BASE.concat('/warehouse')
+export const WAREHOUSE_ENROLL_API_URI = WAREHOUSE_API_URI.concat('/enroll')
 export const WAREHOUSE_UNENROLL_API_URI = WAREHOUSE_API_URI.concat('/unenroll')
 
 export const MAX_AMOUNT_OF_WAREHOUSES_PER_DAY = Number(process.env.NEXT_PUBLIC_MAX_AMOUNT_OF_WAREHOUSES_PER_DAY || '1')
